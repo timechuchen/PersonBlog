cde43d0b00f4e0c6cc3ea4de47c1ad7d2e7f3540
timechuchen <2665300871@qq.com>
2022-02-22 10:52:40 +0800

    利用 nppogress 实现请求进度条功能

--- blog-view/src/api/request.js
+++ blog-view/src/api/request.js
@@ -1,5 +1,9 @@
 //这里需要对 axios 二次封装
 import axios from "axios";
+//引入进度条
+import nprogress from 'nprogress';
+//引入进度条的样式
+import "nprogress/nprogress.css";
 
 //1、利用 Axios 对象的 create 去创建一个 axios 实例
 const requests = axios.create({
@@ -12,15 +16,19 @@ const requests = axios.create({
 //请求拦截器，在请求发送之前做的一些事
 requests.interceptors.request.use((config)=>{
     //config 配置对象，对象里面有一个属性很重要，就是请 headers 求头
+    //进度条开始动
+    nprogress.start();
     return config;
 })
 //响应拦截器
 requests.interceptors.response.use((res)=>{
     //成功的回调函数，服务器响应数据回来之后，响应拦截器可以检测到
+    //进度条结束
+    nprogress.done();
     return res.data;
 },(error)=>{
     //响应失败的回调函数
-    return Promise.reject(new Error('faile'));
+    return Promise.reject(new Error('faile')+''+error);
 })
 
 //对外暴露
