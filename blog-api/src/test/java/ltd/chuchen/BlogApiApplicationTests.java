d00c4b475ddaf83cb21ccfae0f31562a1c31eb17
timechuchen <2665300871@qq.com>
2022-03-02 08:42:48 +0800

    创建 SpringBoot 后端项目

---
+++ blog-api/src/test/java/ltd/chuchen/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.chuchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
